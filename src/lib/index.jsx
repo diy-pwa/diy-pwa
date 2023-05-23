ede92d9caad7ffbe260136d69d90e7fe35702bc6
Rich Hildred <rhildred@gmail.com>
2023-05-23 13:12:36 -0400

    add main

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,9 +1,10 @@
 import Nav from '../../components/Nav.jsx';
 import '../../components/Nav.css';
+import Main from '../../components/Main.jsx';
 import Footer from '../../components/Footer.jsx';
 import '../../components/Footer.css';
 import Contact from '../../components/Contact.jsx';
 import '../../components/Contact.css';
 import HomeHeader from '../../components/HomeHeader.jsx';
 import '../../components/HomeHeader.css'
-export { Nav, Footer, Contact, HomeHeader };
+export { Nav, Main, Footer, Contact, HomeHeader };
