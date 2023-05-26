288e754915c0e284789440ea5fa0f63617211237
Rich Hildred <rhildred@wlu.ca>
2023-05-26 14:41:00 -0400

    get product folder languages from package.json

--- __tests__/ProductLoader.test.js
+++ __tests__/ProductLoader.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, beforeEach } from 'vitest';
 import ProductLoader from "../src/ProductLoader.js";
+import ZipLoader from "../src/ZipLoader.js";
 import fs from 'fs';
 
 const testFolder = "test2";
@@ -10,9 +11,14 @@ beforeEach(async()=>{
 
 describe("Product Loader Creates product pages and data", () => {
     it("creates a list of product pages", async () => {
+        let oZipLoader = new ZipLoader();
+        await oZipLoader.load("https://github.com/diy-pwa/coming-soon/archive/refs/heads/main.zip", testFolder);
+        await oZipLoader.unzip();
+
         const sName = "Test Product";
         let oProductLoader = new ProductLoader({products: [{name: sName}], folder:testFolder});
         await oProductLoader.createProductPages();
         expect(fs.existsSync(`${testFolder}/pages/en/products/${oProductLoader.slugify(sName)}`)).toBe(true);
+        expect(fs.existsSync(`${testFolder}/pages/fr/products/${oProductLoader.slugify(sName)}`)).toBe(true);
     });
 });
--- src/ProductLoader.js
+++ src/ProductLoader.js
@@ -12,9 +12,13 @@ export default class{
         return sName.replace(" ", "_");
     }
     createProductPages(){
-        for(let product of this.products){
-            const sFolderName = this.slugify(product.name);
-            fs.mkdirSync(`${this.folder}/pages/en/products/${sFolderName}`, { recursive: true });
+        const oConfig = JSON.parse(fs.readFileSync(`${this.folder}/package.json`));
+        let aLanguages = oConfig["io_github_diy-pwa_languages"];
+        for(let sLanguage of aLanguages){
+            for(let product of this.products){
+                const sFolderName = this.slugify(product.name);
+                fs.mkdirSync(`${this.folder}/pages/${sLanguage}/products/${sFolderName}`, { recursive: true });
+            }    
         }
     }
 }
