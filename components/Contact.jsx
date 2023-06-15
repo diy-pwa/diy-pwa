5ab8c399f47ebea48bd0674c9aae2d41e193a70d
Rich Hildred <rhildred@gmail.com>
2023-06-15 14:52:02 -0400

    add react code to contact form to improve user experience

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,24 +2,68 @@ import { ReactComponent as PaperAirplane} from './svgs/regular/paper-plane.svg';
 import { ReactComponent as User} from './svgs/regular/user.svg';
 import { ReactComponent as Envelope} from './svgs/regular/envelope.svg';
 import { ReactComponent as Pen} from './svgs/solid/pen.svg';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default (props) => {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const res= await fetch('/api/email', {
+            method: 'POST',
+            body: JSON.stringify(inputs),
+            headers: new Headers({
+              'Content-Type': 'application/json; charset=UTF-8'
+            })
+        });
+        toast.success(props.data.contact.thank_you, {
+            position: toast.POSITION.BOTTOM_CENTER
+        });
+    }
+
+
     return(
-    <form id="contact" className="w3-col s12" action="/api/contact" method="post">
+    <form id="contact" className="w3-col s12" onSubmit={handleSubmit}>
         <div className="w3-row">
             <div className="input-field w3-col m6">
                 <User />
-                <input id="icon_prefix" name="name" type="text" className="validate white-text" />
+                <input id="icon_prefix" 
+                    name="name" 
+                    type="text"
+                    className="validate white-text" 
+                    value={inputs.name || ""}
+                    onChange={handleChange}
+                />
                 <label htmlFor="icon_prefix">{props.data.contact.name}</label>
             </div>
             <div className="input-field w3-col m6">
                 <Envelope />
-                <input id="icon_email" name="email" type="email" className="validate white-text" />
+                <input 
+                    id="icon_email" 
+                    name="email" 
+                    type="email" 
+                    className="validate white-text" 
+                    value={inputs.email || ""}
+                    onChange={handleChange}
+                />
                 <label htmlFor="icon_email">{props.data.contact.email}</label>
             </div>
             <div className="input-field w3-col s12">
                 <Pen />
-                <textarea id="icon_prefix2" name="message" className="materialize-textarea white-text"></textarea>
+                <textarea 
+                    id="icon_prefix2" 
+                    name="message" className="materialize-textarea white-text"
+                    value={inputs.message || ""}
+                    onChange={handleChange}                
+                ></textarea>
                 <label htmlFor="icon_prefix2">{props.data.contact.message}</label>
             </div>
             <div className="w3-row">
@@ -30,6 +74,7 @@ export default (props) => {
                 </div>
             </div>
         </div>
+        <ToastContainer />
     </form>
     );
 }
