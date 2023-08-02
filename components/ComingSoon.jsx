87e92f32f505c72a3b6f12602fc868ea6a79c2b7
rhildred <rhildred@gmail.com>
2023-08-01 21:47:30 -0400

    add ComingSoon component

---
+++ components/ComingSoon.jsx
@@ -0,0 +1,21 @@
+import "w3-css/w3.css";
+import "./ComingSoon.css";
+import {DaysLeft} from "diy-pwa/components"
+
+export default (args)=>{
+    return( 
+    <section id="main" style={{ 
+        backgroundImage: `url(${args.backgroundImage})` 
+      }}>
+        <header className="w3-display-topleft w3-padding-large w3-xlarge">{args.logo}</header>
+        <section className="w3-display-middle">
+        <h1>{args.title}</h1>
+        <hr className="w3-border-grey" />
+        {args.tagline}
+        </section>
+        <footer className="w3-display-bottomleft w3-padding-large">
+        {args.footer} <a href="https://www.w3schools.com/w3css/default.asp" target="_blank"> w3.css</a>
+        </footer>
+      </section>
+      )
+}
---
+++ src/stories/ComingSoon.stories.jsx
@@ -0,0 +1,29 @@
+import ComingSoon from "../../components/ComingSoon";
+import {DaysLeft} from "diy-pwa/components";
+
+export default{
+    component: ComingSoon,
+    title: "components/ComingSoon",
+    args: ['autodocs'],
+}
+
+export const en ={
+    args:{
+        title: "Coming Soon",
+        tagline: <h2> ... in <DaysLeft readyDate="2023-09-05" /> days</h2>,
+        backgroundImage: "/img/forestbridge.jpg",
+        logo: "Logo",
+        footer: "Powered by"
+    }
+}
+
+export const fr ={
+    args:{
+        title: "Bientôt disponible",
+        tagline: <h2> ... dans <DaysLeft readyDate="2023=09-05" /> jours</h2>,
+        backgroundImage: "/img/forestbridge.jpg",
+        logo: "Logo",
+        footer: "Alimenté par"
+    }
+}
+
