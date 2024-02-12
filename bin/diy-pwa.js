5f6fe8902cc9ba355ee07dfb70687b317ce73c1a
Rich Hildred <rhildred@wlu.ca>
2024-02-11 20:11:18 -0500

    diy-pwa is a wordpress bundler

---
+++ bin/diy-pwa.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import {PhpLoader} from '../lib/index.js';
+import path from "node:path";
+
+const sProg = path.basename(process.argv[1]);
+const sCommand = process.argv[2];
+const oLoader = new PhpLoader();
+let app = null;
+switch(sCommand){
+    case "build":
+        await oLoader.build(process.argv[3]);
+        break;
+    case "preview":
+        app = await oLoader.preview(process.argv[3]);
+        break;
+    default:
+        console.log(`${sProg} (build|preview|dev|start) ?output_folder`);
+}
+if(app){
+    const server = app.listen(8000, ()=>{
+        console.log(`\`${sProg} ${sCommand}\` is listening on port ${server.address().port}`);
+    })
+}else{
+    process.exit();
+}
