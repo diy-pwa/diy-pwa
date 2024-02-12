b82c575e35e6d467d16ccd15a8fbab856aa5e35e
Rich Hildred <rhildred@wlu.ca>
2024-02-11 21:11:39 -0500

    remove a couple more

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ Plugin Name: diy-pwa
 */
 
 add_action('admin_init', function(){
-    remove_meta_box('dashboard_primary', 'dashboard', 'normal'); //Removes the 'WordPress News' widget    
+    remove_meta_box('dashboard_primary', 'dashboard', 'normal'); //Removes the 'WordPress News' widget
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side'); //Removes the 'Recent Drafts' widget
+    remove_meta_box('dashboard_site_health', 'dashboard', 'normal'); // Remove site health wizard
 });
 
 
