2f1a31ba000592e0d398d581a34aa0c557936fb6
Rich Hildred <rhildred@wlu.ca>
2024-02-11 21:15:23 -0500

    quick draft not recent draft

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: diy-pwa
 
 add_action('admin_init', function(){
     remove_meta_box('dashboard_primary', 'dashboard', 'normal'); //Removes the 'WordPress News' widget
-    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side'); //Removes the 'Recent Drafts' widget
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side'); //Removes the 'Quick Draft' widget
     remove_meta_box('dashboard_site_health', 'dashboard', 'normal'); // Remove site health wizard
 });
 
