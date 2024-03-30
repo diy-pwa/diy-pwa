1613c0547f8bcb123a61fbc91d731cac8c4f9285
Rich Hildred <rhildred@wlu.ca>
2024-03-30 12:19:09 -0400

    new version of diy-pwa with simplified .zip management

--- spec/WordpressLoader.spec.js
+++ spec/WordpressLoader.spec.js
@@ -1,16 +1,23 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { WordpressLoader } from "../lib";
-import { ZipLoader } from "../lib";
+import { Zip } from "../lib";
 import fs from 'fs';
 
 const CORSPROXY = "https://wp-now-corsproxy.rhildred.workers.dev/corsproxy";
-
+const outDir = "test";
 describe("tests WordpressLoader", ()=>{
-    it("makes a static bundle", async ()=>{
-        const oZipLoader = new ZipLoader({fs});
-        const outDir = "test"; 
-        await oZipLoader.load(`${CORSPROXY}/github.com/emilyychau/UX221-L06/archive/refs/heads/main.zip`, outDir);
-        await oZipLoader.unzip();
+    beforeEach(async()=>{
+        try{
+            await fs.promises.rmdir(outDir, {recursive: true});
+        }catch{}
+    })
+    it("makes a static bundle from student1", async ()=>{
+        const oZip = new Zip({fs, 
+            dest:outDir,
+            url: `${CORSPROXY}/github.com/emilyychau/UX221-L06/archive/refs/heads/main.zip`,
+            filter: 'wp-config.php'
+            });
+        await oZip.unzip();
         const oWPLoader = new WordpressLoader({fs, outDir});
         await oWPLoader.build("test");
         try{
@@ -20,5 +27,22 @@ describe("tests WordpressLoader", ()=>{
             expect(true).toBe(false);
         }
 
-    }, 40000);
+    }, 30000);
+    it("makes a static bundle from student2", async ()=>{
+        const oZip = new Zip({fs,
+            dest: outDir,
+            url:`${CORSPROXY}/github.com/rhildred/paintball/archive/refs/heads/main.zip`,
+            filter:"wp-config.php"
+        });
+        await oZip.unzip();
+        const oWPLoader = new WordpressLoader({fs, outDir});
+        await oWPLoader.build("test");
+        try{
+            await fs.promises.access("test/dist/index.html");
+            expect(true).toBe(true);
+        }catch{
+            expect(true).toBe(false);
+        }
+
+    }, 30000);
 });
