6f531a46d0eddb4ccfa6001e7cbde5a9c0bc5e5d
Rich Hildred <rhildred@gmail.com>
2025-02-16 13:30:59 -0500

    made it work except for formatting

--- bin/diy-pwa.js
+++ bin/diy-pwa.js
@@ -2,8 +2,9 @@
 import process from "node:process";
 import path from "node:path";
 // An import assertion in a static import
-import info from '../package.json' with { type: "json" };
-import {sitemap} from '../lib/sitemap.js';
+import info from "../package.json" with { type: "json" };
+import {sitemap} from "../lib/sitemap.js";
+import {createSearch} from "../lib/createSearch.js";
 
 const sProg = path.basename(process.argv[1]);
 const sCommand = process.argv[2];
@@ -12,13 +13,13 @@ const outDir = process.argv[4] || ".";
 let app = null;
 switch(sCommand){
     case "sitemap":
-        sitemap({root:process.cwd(), publicDir:""});
+        sitemap({root:process.cwd(), publicDir:outDir});
         break;
     case "index":
-        // this is where we will do the index diy-pwa.json
+        createSearch(outDir);
         break;
     default:
-        console.log(`${sProg} (index) ?output_folder`);
+        console.log(`${sProg} (index|sitemap) ?output_folder`);
         process.exit();
 }
  
--- search.js
+++ search.js
@@ -1,10 +1,21 @@
+import lunr from 'https://cdn.jsdelivr.net/npm/lunr@2.3.9/+esm'
+
 class SearchResults extends HTMLElement{
     async connectedCallback(){
         const urlParams = new URLSearchParams(window.location.search);
         if(urlParams.has("s") && urlParams.get("s") != ""){
             this.oldStyle = document.querySelector("main").style.display;
             document.querySelector("main").style.display = "none";
-            this.innerHTML = `search results for ${urlParams.get("s")} here`
+            const res = await fetch("searchindex.json");
+            const idx = lunr.Index.load(await res.json());
+            const sSearchString = urlParams.get("s");
+            const oResults = idx.search(sSearchString);
+            if(oResults.length){
+                this.innerHTML = `search results for ${sSearchString} here`    
+            }else{
+                this.innerHTML = `no search results for ${sSearchString}`
+            }
+            
         }else{
             document.querySelector("main").style.display = this.oldStyle;
         }
