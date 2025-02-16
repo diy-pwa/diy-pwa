b6aed4b5edac94fc1fb2d2515de8d1487edf5d25
Rich Hildred <rhildred@gmail.com>
2025-02-15 19:42:17 -0500

    make sitemap

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ TLDR;
 
 ```bash
 npx diy-pwa index
+```
+
--- bin/diy-pwa.js
+++ bin/diy-pwa.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-
+import process from "node:process";
 import path from "node:path";
 // An import assertion in a static import
 import info from '../package.json' with { type: "json" };
+import {sitemap} from '../lib/sitemap.js';
 
 const sProg = path.basename(process.argv[1]);
 const sCommand = process.argv[2];
@@ -10,6 +11,9 @@ console.log(`${sProg} ${sCommand||"none"} version ${info.version}`)
 const outDir = process.argv[4] || ".";
 let app = null;
 switch(sCommand){
+    case "sitemap":
+        sitemap({root:process.cwd(), publicDir:""});
+        break;
     case "index":
         // this is where we will do the index diy-pwa.json
         break;
