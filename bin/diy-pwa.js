70904e51f5f816a9e7bac991358eb5007c92c07c
Rich Hildred <rhildred@gmail.com>
2025-02-15 19:06:19 -0500

    test with npx

--- bin/diy-pwa.js
+++ bin/diy-pwa.js
@@ -1,46 +1,20 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import {WordpressLoader} from '../lib/index.js';
 import path from "node:path";
-// An import assertion in a static import
-import info from '../package.json' assert { type: "json" };
-import {spawn} from 'child_process';
+import fs from "node:fs";
 
-
+const info = JSON.parse(fs.readFileSync("./package.json"))
 const sProg = path.basename(process.argv[1]);
 const sCommand = process.argv[2];
-console.log(`${sProg} ${sCommand} version ${info.version}`)
+console.log(`${sProg} ${sCommand||"none"} version ${info.version}`)
 const outDir = process.argv[4] || ".";
-const oLoader = new WordpressLoader({fs, outDir});
 let app = null;
 switch(sCommand){
-    case "build":
-        setTimeout(async ()=>{
-            await oLoader.build(process.argv[3]);
-            process.exit();
-        }, 10000);
-        spawn('node', [process.argv[1], "setup", outDir], {
-            detached: true
-        });
+    case "index":
+        // this is where we will do the index diy-pwa.json
         break;
-    case "preview":
-        setTimeout(async ()=>{
-            app = await oLoader.preview(process.argv[3]);
-            const server = app.listen(8000, ()=>{
-                console.log(`\`${sProg} ${sCommand}\` is listening on port ${server.address().port}`);
-            });
-        }, 10000);
-        spawn('node', [process.argv[1], "setup", outDir], {
-            detached: true
-        });
-        break;
-    case "setup":
-        await oLoader.setup(process.argv[3]);
-        process.exit();
-    case "fork":
     default:
-        console.log(`${sProg} (build|preview|dev|start) ?output_folder`);
+        console.log(`${sProg} (index) ?output_folder`);
         process.exit();
 }
  
---
+++ spec/Sitemap.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+
+describe("tests sitemap", ()=>{
+    it("true is true and ever so shall be so", async ()=>{
+        expect(true).toBe(true);
+    });
+});
+
