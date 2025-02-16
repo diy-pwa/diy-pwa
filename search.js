48d78267724cc8108124f3636ec46a5074a7bc57
Rich Hildred <rhildred@gmail.com>
2025-02-16 14:53:32 -0500

    display results

--- search.js
+++ search.js
@@ -1,23 +1,30 @@
-import lunr from 'https://cdn.jsdelivr.net/npm/lunr@2.3.9/+esm'
+import lunr from "https://cdn.jsdelivr.net/npm/lunr@2.3.9/+esm";
+import * as cheerio from "https://cdn.jsdelivr.net/npm/cheerio@1.0.0/+esm";
 
 class SearchResults extends HTMLElement{
     async connectedCallback(){
         const urlParams = new URLSearchParams(window.location.search);
         if(urlParams.has("s") && urlParams.get("s") != ""){
-            this.oldStyle = document.querySelector("main").style.display;
-            document.querySelector("main").style.display = "none";
             const res = await fetch("searchindex.json");
             const idx = lunr.Index.load(await res.json());
             const sSearchString = urlParams.get("s");
             const oResults = idx.search(sSearchString);
             if(oResults.length){
-                this.innerHTML = `search results for ${sSearchString} here`    
+                this.insertAdjacentHTML("beforeend", `search results for ${sSearchString} here`);
+                for(let oResult of oResults){
+                    const res = await fetch(oResult.ref);
+                    const sText = await res.text();
+                    const $ = cheerio.load(sText);
+                    $("script").remove();
+                    $("style").remove();
+                    const sTitle = $("h1").text().replace(/\s+/g, ' ').trim();
+                    const sBody = $("body").text().replace(/\s+/g, ' ').trim().replace(sTitle, "");
+                    this.insertAdjacentHTML("beforeend",`<a href="${oResult.ref}"><h2>${sTitle}</h2></a><p>${sBody}</p>`);
+                }
             }else{
                 this.innerHTML = `no search results for ${sSearchString}`
             }
             
-        }else{
-            document.querySelector("main").style.display = this.oldStyle;
         }
     }
 }
